160d0a39ee1c9708686aa7ab1fa2a36fbcc11115
Giorgi (orb) Orbeliani <g.orbeliani@expl.one>
2025-12-20 19:27:39 +0000

    feat: add custom media query variants for pointer types and PWA display mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,17 @@ const defaultTheme = {
   container: {
     center: true,
     padding: "2rem",
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+    },
   },
   extend: {
     screens: {
       xs: "400px",
-      coarse: { raw: "(pointer: coarse)" },
-      fine: { raw: "(pointer: fine)" },
-      pwa: { raw: "(display-mode: standalone)" },
     },
     colors: {
       neutral: {
